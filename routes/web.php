e930f1c48bb58b864ce7cb21d42b6ea89659df14
unknown <radek15855@gmail.com>
2018-06-11 16:03:23 +0200

    task

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+    Route::get('/', 'TaskController@index');
+
+    Route::post('/task/create','TaskController@store')->name('task.create');
+
+    Route::delete('/task/delete/{task}', 'TaskController@destroy')->name('task.destroy');
